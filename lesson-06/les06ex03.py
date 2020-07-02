a614ac3676f8423dda0797dfb2aa3253ce876db3
Dennis Veselov <wesselloff@gmail.com>
2020-07-02 03:59:38 +0300

    Задачи к пятому уроку доделаны

--- lesson-06/les06ex03.py
+++ lesson-06/les06ex03.py
@@ -1,4 +1,36 @@
 #!/usr/bin/python3
 
-#
+# 3. Реализовать базовый класс Worker (работник), в котором определить атрибуты:
+#    name, surname, position (должность), income (доход).
+#    Последний атрибут должен быть защищенным и ссылаться на словарь, содержащий элементы: оклад и премия,
+#    например, {"wage": wage, "bonus": bonus}.
+#    Создать класс Position (должность) на базе класса Worker. В классе Position реализовать методы получения
+#    полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+#    Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+#    проверить значения атрибутов, вызвать методы экземпляров).
 
+
+class Worker:
+    def __init__(self, name, surname, position, rate, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'rate': rate, 'bonus': bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.surname} {self.name}'
+
+    def get_total_income(self):
+        try:
+            return sum(self._income.values())
+        except TypeError:
+            return 'Некорректные данные по доходам сотрудника'
+
+
+Vasya = Position('Вася', 'Пупкин', 'Отвечающий за всё', 10000, 5000)
+#Vasya = Position('Вася', 'Пупкин', 'Отвечающий за всё', 'lskdjf', 5000)
+print('Полное имя', Vasya.get_full_name())
+print('Должность', Vasya.position)
+print('Доход', Vasya.get_total_income())
--- lesson-06/les06ex05.py
+++ lesson-06/les06ex05.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python3
 
-#
+# 5. Реализовать класс Stationery (канцелярская принадлежность).
+#    Определить в нем атрибут title (название) и метод draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.”
+#    Создать три дочерних класса Pen (ручка), Pencil (карандаш), Handle (маркер).
+#    В каждом из классов реализовать переопределение метода draw.
+#    Для каждого из классов методы должен выводить уникальное сообщение.
+#    Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 
+
+class Stationery:
+    def __init__(self, title='Оно рисует'):
+        self.title = title
+
+    def draw(self):
+        print(f'Запуск отрисовки - {self.title}')
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f'Рисуем ручкой {self.title}.')
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f'Рисуем карандашом {self.title}.')
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f'Рисуем маркером {self.title}.')
+
+
+thing = Stationery()
+thing.draw()
+
+pen = Pen('BIC')
+pen.draw()
+
+pencil = Pencil('Конструктор')
+pencil.draw()
+
+marker = Handle('Permanent')
+marker.draw()
